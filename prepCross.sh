1b33e51db9483ff7d6275464b35d0a3e2884e206
Christopher Lewis <clew1996@gmail.com>
2021-05-03 22:34:46 -0700

    Added script to setup cross-compiler...Hopefully

---
+++ prepCross.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+mkdir cross 
+cd cross
+
+
+#SET UP ENV VARIABLES
+
+export PREFIX="$(pwd)"
+export TARGET=i686-elf
+export PATH="$PREFIX/bin:$PATH"
+
+#FOR BUILD UTILITIES INSTALL
+
+wget https://ftp.gnu.org/gnu/binutils/binutils-2.30.tar.gz
+tar -xvf binutils-2.30.tar.gz
+mkdir build-binutils
+cd build-binutils
+
+../binutils-2.30/configure --target=$TARGET --prefix="$PREFIX" --disable-nls 
+
+make && make install
+
+cd ..
+
+#For GCC INSTALL
+
+wget https://ftp.gnu.org/gnu/gmp/gmp-5.1.3.tar.gz
+
+wget https://ftp.gnu.org/gnu/mpfr/mpfr-4.1.0.tar.gz
+
+wget https://ftp.gnu.org/gnu/mpc/mpc-1.2.1.tar.gz
+
+tar  -xvf gmp-5.1.3.tar.gz
+tar -xvf mpfr-4.1.0.tar.gz
+tar -xvf mpc-1.2.1.tar.gz
+
+mv gmp-5.1.3 gcc-7.5.0/gmp
+mv mpfr-4.1.0 gcc-7.5.0/mpfr
+mv mpc-1.2.1 gcc-7.5.0/mpc
+
+
+make all-gcc && make all-target-libgcc && make install-gcc && make install-target-libgcc
+
+
+exit 0
